25c6a6f98b27dc24b917442eae66f2850ae5e6fd
H. R. Wells <100534001+H-R-Wells@users.noreply.github.com>
2022-04-26 21:56:03 +0530

    till clock

--- js/js10.js
+++ js/js10.js
@@ -1,4 +1,35 @@
-// SetTimeOut and setInterval
+// setTimeOut and setInterval
 function clicked(){
     alert("Thank You for clicking bitch")
 }
+
+function summ(a,b){
+    return a+b;
+}
+
+sum2 = (a,b) => {
+    return a+b;
+}
+
+log = () =>{
+    document.getElementById('firstContainer').innerText = "Ok It's Working Bitch"
+}
+
+intv = () =>{
+    console.log("Ok Great")
+}
+setTimeout(log, 4000);
+sint = setInterval(intv, 4000);
+
+// localStorage
+localStorage.setItem('name', 'Shubham');
+localStorage.getItem('name');
+localStorage
+
+// JSON
+obj = {'name':'Shubham', a :{"MDFK":"Chaman"}};
+jso = JSON.stringify(obj);
+console.log(typeof jso);
+console.log(jso);
+console.log(obj);
+console.log(typeof obj);
