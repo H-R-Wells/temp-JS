35819aaf2eb0e057d6b65f22338d6ac50221c552
H. R. Wells <100534001+H-R-Wells@users.noreply.github.com>
2022-05-02 18:09:31 +0530

    reg ex

--- js/js11.js
+++ js/js11.js
@@ -1,25 +1,37 @@
-// Regular expression
-let reg = /sunny/i;
-let reg2 = /sunny/g;
+    // Regular expression
+// let reg = /sunny/i;
+// let reg2 = /sunny/g;
 
-console.log(reg);
-let s = "Hello MDFK the big sunny bitch sunny";
-// let result = reg.exec(s);
-let result2 = reg2.exec(s);
-console.log(result2);
-result2 = reg2.exec(s);
-// console.log(result);
-console.log(result2);
-result2 = reg2.exec(s);
-console.log(result2);
-if (result2){
-console.log(result2.input);
-console.log(result2.index);
+// console.log(reg);
+// let s = "Hello MDFK the big sunny bitch sunny";
+// // let result = reg.exec(s);
+// let result2 = reg2.exec(s);
+// console.log(result2);
+// result2 = reg2.exec(s);
+// // console.log(result);
+// console.log(result2);
+// result2 = reg2.exec(s);
+// console.log(result2);
+// if (result2){
+// console.log(result2.input);
+// console.log(result2.index);
+// }
+
+
+// let result3 = s.match(reg2);
+// console.log(result3);
+
+// let result4 = s.search(reg2);
+// console.log(result4);
+
+let str = "Hello bitch how is it going?"
+let regex = /h[o]w/;
+if (regex.test(str)){
+    console.log(`regex ${str} contains ${regex.source}`);
+}
+else{
+    console.log(`regex ${str} does not contai ${regex.source}`);
 }
 
-
-let result3 = s.match(reg2);
-console.log(result3);
-
-let result4 = s.search(reg2);
-console.log(result4);
+let result = regex.exec(str);
+console.log(result);
