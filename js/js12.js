865a5313bba803f4fddc6e5e7d9a64ee465bc0ce
H. R. Wells <100534001+H-R-Wells@users.noreply.github.com>
2022-06-08 19:50:40 +0530

    Update

--- js/js12.js
+++ js/js12.js
@@ -1,1 +1,11 @@
 console.log("OK")
+let a = location.href;
+a = document.links
+console.log(document.links[1]);
+
+let cont = document.querySelector('.no');
+cont = document.querySelector('.container');
+
+let e = document.getElementById("chaman")
+e.style.color = "red"
+console.log(e);
